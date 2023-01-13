80fc527c1b44775334e5852a7715a9ffdab075d6
cgtnt <tntmynamemetak@gmail.com>
2023-01-13 16:58:04 +0100

    stable v3

--- arduinoSide/arduinoSide.ino
+++ arduinoSide/arduinoSide.ino
@@ -1,77 +1,150 @@
 //PIN VARIABLES - CHANGE THIS
-int red_light_pin= 9;
+int red_light_pin = 9;
 int green_light_pin = 10;
 int blue_light_pin = 11;
 
+
+//OTHER VARIABLES - DO NOT TOUCH
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+
+int r1;
+int g1;
+int b1;
+int grad;
+int r2;
+int g2;
+int b2;
+int rc;
+int gc;
+int bc;
+
+bool rclimb;
+bool gclimb;
+bool bclimb;
+
+//Setup
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(red_light_pin, OUTPUT);
   pinMode(green_light_pin, OUTPUT);
   pinMode(blue_light_pin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  if(Serial.available()>0){   //Reads serial input from node.js server
-    String receivedData = Serial.readString();  
-    int dataLength = receivedData.length()+1;
-    char charArray[dataLength];
-    receivedData.toCharArray(charArray, dataLength);
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char *strings[8];
+  char *ptr;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rcc;
+  //Serial reading
+  while (Serial.available() > 0 && newData == false) {
+    rcc = Serial.read();
 
-    char *strings[7];
-    char *ptr = NULL;
+    if (recvInProgress == true) {
+      if (rcc != endMarker) {
+        receivedChars[ndx] = rcc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0';
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rcc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+  //String parsing
+  if (newData == true) {
+    //digitalWrite(LED_BUILTIN, HIGH);
     byte index = 0;
-    ptr = strtok(charArray, ",");
-    while (ptr != NULL)
+    ptr = strtok(receivedChars, ",");
+    while (ptr != '\0')
     {
       strings[index] = ptr;
       index++;
-      ptr = strtok(NULL, ",");
+      ptr = strtok('\0', ",");
     }
-    int r1 = atoi(strings[0]);
-    int g1 = atoi(strings[1]);
-    int b1 = atoi(strings[2]);
-    if(String(strings[3])=="gr"){
-      int r2 = atoi(strings[4]);
-      int g2 = atoi(strings[5]);
-      int b2 = atoi(strings[6]);
 
-      int rc = r1;
-      int gc = g1;
-      int bc = b1;
+    r1 = atoi(strings[0]);
+    g1 = atoi(strings[1]);
+    b1 = atoi(strings[2]);
+    grad = atoi(strings[3]);
+    r2 = atoi(strings[4]);
+    g2 = atoi(strings[5]);
+    b2 = atoi(strings[6]);
 
-      bool rclimb = true;
-      bool gclimb = true;
-      bool bclimb = true;
+    if (grad == 1) {
+      rc = r1;
+      gc = g1;
+      bc = b1;
 
-      while (Serial.available()==0){
-         delay(25);
-         
-         if(rc==r1){rclimb=true;}else if(rc==r2){rclimb=false;}
-         if(gc==g1){gclimb=true;}else if(gc==g2){gclimb=false;}
-         if(bc==b1){bclimb=true;}else if(bc==b2){bclimb=false;}
-
-         if (r1!=r2){
-           if(rclimb==true){rc++;}else{rc--;}
-         }
-         if (g1!=g2){
-           if(gclimb==true){gc++;}else{gc--;}
-         }
-         if (b1!=b2){
-           if(bclimb==true){bc++;}else{bc--;}
-         }
-         analogWrite(red_light_pin, rc);
-         analogWrite(green_light_pin, gc);
-         analogWrite(blue_light_pin, bc);
-         Serial.write(strings[3]);
-      }
-    }else{
+      rclimb = true;
+      gclimb = true;
+      bclimb = true;
+    } else {
+      
       analogWrite(red_light_pin, r1);
       analogWrite(green_light_pin, g1);
-      analogWrite(blue_light_pin, b1);
-      
-      Serial.println(r1);
-    Serial.println(g1);
-    Serial.println(b1);
+      analogWrite(blue_light_pin, b1); 
     }
+    newData = false;
+  }
+  //Gradient
+  if (grad == 1) {
+    delay(25);
+
+    if (rc == min(r1,r2)) {
+      rclimb = true;
+    } else if (rc == max(r1,r2)) {
+      rclimb = false;
+    }
+    if (gc == min(g1,g2)) {
+      gclimb = true;
+    } else if (gc == max(g1,g2)) {
+      gclimb = false;
+    }
+    if (bc == min(b1,b2)) {
+      bclimb = true;
+    } else if (bc == max(b1,b2)) {
+      bclimb = false;
+    }
+
+    if (r1 != r2) {
+      if (rclimb == true) {
+        rc++;
+      } else {
+        rc--;
+      }
+    }
+    if (g1 != g2) {
+      if (gclimb == true) {
+        gc++;
+      } else {
+        gc--;
+      }
+    }
+    if (b1 != b2) {
+      if (bclimb == true) {
+        bc++;
+      } else {
+        bc--;
+      }
+    }
+    analogWrite(red_light_pin, rc);
+    analogWrite(green_light_pin, gc);
+    analogWrite(blue_light_pin, bc); 
   }
 }
